93cd841d37d701e4fc7748af8340d47b67af4b62
Vini <visilva5690@hotmail.com>
2024-07-16 23:58:49 -0300

    Aprendendo a criar filtros com o camel

--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -6,22 +6,29 @@ import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		CamelContext context = new DefaultCamelContext();
+        CamelContext context = new DefaultCamelContext();
 
-		context.addRoutes(new RouteBuilder() {
+        context.addRoutes(new RouteBuilder() {
 
-			@Override
-			public void configure() throws Exception {
-				from("file:pedidos?delay=5s&noop=true").
-						log("${id} - ${body}").
-						to("file:saida");
-			}
-		});
+            @Override
+            public void configure() throws Exception {
+                from("file:pedidos?delay=5s&noop=true").
+                        split(). // divide o conteudo
+                            xpath("/pedido/itens/item").
+                        filter().
+                            xpath("/item/formato[text()='EBOOK']"). // filtra as mensagens
+                        log("${id}").
+                        marshal().xmljson().
+                        log("${body}").
+                        setHeader("CamelFileName", simple("${file:name.noext}.toJson")).
+                to("file:saida");
+            }
+        });
 
-		context.start(); //aqui camel realmente começa a trabalhar
-		Thread.sleep(20000);
-		context.stop();
-	}
+        context.start(); //aqui camel realmente começa a trabalhar
+        Thread.sleep(20000);
+        context.stop();
+    }
 }
